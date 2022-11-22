39471dc67c6bb64d2a92d32581d2a4929fb13ca1
banushi-a <altonbanushi@icloud.com>
2022-11-22 10:55:16 -0500

    Changed create_db.sql to more closely align with ERD

--- db_bootstrap/create_db.sql
+++ db_bootstrap/create_db.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE form_now_db;
 CREATE USER 'webapp'@'%' IDENTIFIED BY 'abc123';
-GRANT ALL PRIVILEGES ON cool_db.* TO 'webapp'@'%';
+GRANT ALL PRIVILEGES ON form_now_db.* TO 'webapp'@'%';
 FLUSH PRIVILEGES;
 
 USE form_now_db;
@@ -32,54 +32,54 @@ CREATE TABLE Analysts (
 
 CREATE TABLE Forms (
     FormId INTEGER PRIMARY KEY,
-    CreatorUsername VARCHAR(20) FOREIGN KEY,
-    FormName VARCHAR(40)
+    CreatorUsername VARCHAR(20),
+    FormName VARCHAR(40),
+    FOREIGN KEY (CreatorUsername) REFERENCES Creators(CreatorUsername)
+);
+
+CREATE TABLE Questions (
+    QuestionId INTEGER PRIMARY KEY,
+    FormId INTEGER,
+    QuestionText VARCHAR(200),
+    RequiresResponse BOOLEAN,
+    QuestionWeight INTEGER,
+    FOREIGN KEY (FormId) REFERENCES Forms(FormId)
 );
 
 CREATE TABLE MCQuestions (
     MCQuestionId INTEGER PRIMARY KEY,
-    FormId INTEGER FOREIGN KEY,
-    MCQuestionText VARCHAR(200),
-    MCQuestionWeight INTEGER,
-    RequiresResponse BOOLEAN
+    FOREIGN KEY (MCQuestionId) REFERENCES Questions(QuestionId)
 );
 
 CREATE TABLE MCQuestionPossibilities (
     MCQuestionPossibilityId INTEGER PRIMARY KEY,
-    MCQuestionId INTEGER FOREIGN KEY,
+    MCQuestionId INTEGER,
     MCQuestionPossibilityText VARCHAR(100),
-    MCQuestionCorrect BOOLEAN
+    MCQuestionCorrect BOOLEAN,
+    FOREIGN KEY (MCQuestionId) REFERENCES MCQuestions(MCQuestionId)
 );
 
 CREATE TABLE ShortAnswerQuestions (
     ShortAnswerId INTEGER PRIMARY KEY,
-    FormId INTEGER FOREIGN KEY,
-    ShortAnswerQuestionText VARCHAR(200),
-    ShortAnswerQuestionWeight INTEGER,
-    RequiresResponse BOOLEAN
+    FOREIGN KEY (ShortAnswerId) REFERENCES Questions(QuestionId)
 );
 
 CREATE TABLE SliderQuestions (
     SliderId INTEGER PRIMARY KEY,
-    FormId INTEGER FOREIGN KEY,
-    SliderText VARCHAR(200),
-    SliderQuestionWeight INTEGER,
-    RequiresResponse BOOLEAN
+    FOREIGN KEY (SliderId) REFERENCES Questions(QuestionId)
 );
 
 CREATE TABLE CheckboxQuestions (
     CheckboxQuestionId INTEGER PRIMARY KEY,
-    FormId INTEGER FOREIGN KEY,
-    CheckboxQuestionText VARCHAR(200),
-    CheckboxQuestionTotalWeight INTEGER,
-    RequiresResponse BOOLEAN
+    FOREIGN KEY (CheckboxQuestionId) REFERENCES Questions(QuestionId)
 );
 
 CREATE TABLE CheckboxQuestionPossibilities (
     CheckboxQuestionPosibilityId INTEGER PRIMARY KEY,
-    CheckboxQuestionId INTEGER FOREIGN KEY,
+    CheckboxQuestionId INTEGER,
     CheckboxQuestionPosibilityText VARCHAR(100),
-    CheckboxQuestionCorrect BOOLEAN
+    CheckboxQuestionCorrect BOOLEAN,
+    FOREIGN KEY (CheckboxQuestionId) REFERENCES CheckboxQuestions(CheckboxQuestionId)
 );
 
 CREATE TABLE FormsRespondents (
@@ -87,20 +87,35 @@ CREATE TABLE FormsRespondents (
     RespondentUsername VARCHAR(20),
     EarnedWeight INTEGER,
     PossibleWeight INTEGER,
-    PRIMARY KEY(FormId, RespondentUsername)
+    PRIMARY KEY (FormId, RespondentUsername),
+    FOREIGN KEY (FormId) REFERENCES Forms(FormId),
+    FOREIGN KEY (RespondentUsername) REFERENCES Respondents(RespondentUsername)
 );
 
+-- changed this to not be dependent on FormRespondents since we 
+-- can get that information from JOINING up through the tables
+-- + this follows our ER diagram more closely
 CREATE TABLE FormsRespondentsQuestions (
-    FormId INTEGER,
-    RespondentUsername VARCHAR(20),
     QuestionId INTEGER,
+    RespondentUsername VARCHAR(20),
     Answer VARCHAR(200),
-    PRIMARY KEY(FormId, RespondentUsername, QuestionId)
+    PRIMARY KEY (QuestionId, RespondentUsername),
+    FOREIGN KEY (QuestionId) REFERENCES Questions(QuestionId),
+    FOREIGN KEY (RespondentUsername) REFERENCES Respondents(RespondentUsername)
 );
 
 CREATE TABLE AnalystsForms (
     AnalystUsername VARCHAR(20),
     FormId INTEGER,
     Observations VARCHAR(200),
-    PRIMARY KEY(AnalystUsername, FormId)
+    PRIMARY KEY (AnalystUsername, FormId),
+    FOREIGN KEY (FormId) REFERENCES Forms(FormId)
 );
+
+
+-- ADD DATA
+
+INSERT INTO Creators
+    (CreatorUsername, CreatorPassword, Email, FirstName, LastName)
+VALUES
+    ('creator1', 'creator1password', 'creator1@gmail.com', 'creator', 'one');
