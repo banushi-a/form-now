304c48eae1cd5e56cc951d564543c69048776815
banushi-a <altonbanushi@icloud.com>
2022-12-10 16:31:37 -0500

    added form for mcq

--- form_now_app/app.py
+++ form_now_app/app.py
@@ -1,4 +1,5 @@
 # import the Flask framework
+from multiprocessing import current_process
 from flask import Flask, jsonify, request
 from flaskext.mysql import MySQL
 
@@ -61,6 +62,41 @@ def get_respondents():
        json_data.append(dict(zip(row_headers, row)))
    return jsonify(json_data)
 
+@app.route('/respondent-response', methods=['POST'])
+def post_response():
+    app.logger.info(request.form)
+    cursor = db_connection.get_db().cursor()
+    respondentId = request.form['respondent-id']
+    questionId = request.form['question-id']
+    answer = request.form['answer']
+    query = f'INSERT INTO FormsRespondentsQuestions (QuestionId, RespondentUsername, Answer) VALUES (\"{questionId}\", \"{respondentId}\", \"{answer}\")'
+    cursor.execute(query)
+    db_connection.get_db().commit()
+    return "Success!"
+
+@app.route('/questions-mc')
+def get_questions_mc():
+   cur = db_connection.get_db().cursor()
+   cur.execute('select QuestionText as label, Questions.QuestionId as value from Questions join MCQuestions on Questions.QuestionId = MCQuestions.MCQuestionId')
+   row_headers = [x[0] for x in cur.description]
+   json_data = []
+   theData = cur.fetchall()
+   for row in theData:
+       json_data.append(dict(zip(row_headers, row)))
+   return jsonify(json_data)
+
+@app.route('/questions-mc-options')
+def get_questions_mc_options():
+   cur = db_connection.get_db().cursor()
+   mcqID = request.args.get('mcq-id')
+   cur.execute("select MCQuestionPossibilityText as label, MCQuestionPossibilityId as value from Questions join MCQuestions on Questions.QuestionId = MCQuestions.MCQuestionId natural join MCQuestionPossibilities where MCQuestions.MCQuestionId = '" + str(mcqID) +"'")
+   row_headers = [x[0] for x in cur.description]
+   json_data = []
+   theData = cur.fetchall()
+   for row in theData:
+       json_data.append(dict(zip(row_headers, row)))
+   return jsonify(json_data)
+
 @app.route('/creator-forms')
 def get_creator_forms():
     creator = request.args.get('creator-id')
@@ -97,11 +133,67 @@ def get_creator_num_forms():
       json_data.append(dict(zip(row_headers, row)))
   return jsonify(json_data)
 
+@app.route('/respondent-num-forms')
+def get_respondent_num_forms():
+  respondent = request.args.get('respondent-id')
+  cur = db_connection.get_db().cursor()
+  cur.execute("select count(*) as count from Respondents natural join FormsRespondents natural join Forms where RespondentUsername = '" + str(respondent) +"'")
+  row_headers = [x[0] for x in cur.description]
+  json_data = []
+  theData = cur.fetchall()
+  for row in theData:
+      json_data.append(dict(zip(row_headers, row)))
+  return jsonify(json_data)
+
 @app.route('/creator-num-questions')
 def get_creator_num_questions():
   creator = request.args.get('creator-id')
   cur = db_connection.get_db().cursor()
   cur.execute("select count(*) from Questions natural join Forms where CreatorUsername = '" + str(creator) +"'")
+  row_headers = [x[0] for x in cur.description]
+  json_data = []
+  theData = cur.fetchall()
+  for row in theData:
+      json_data.append(dict(zip(row_headers, row)))
+  return jsonify(json_data)
+
+@app.route('/respondent-num-questions')
+def get_respondent_num_questions():
+  creator = request.args.get('respondent-id')
+  cur = db_connection.get_db().cursor()
+  cur.execute("select count(*) as count from Respondents natural join FormsRespondentsQuestions where RespondentUsername = '" + str(creator) +"'")
+  row_headers = [x[0] for x in cur.description]
+  json_data = []
+  theData = cur.fetchall()
+  for row in theData:
+      json_data.append(dict(zip(row_headers, row)))
+  return jsonify(json_data)
+
+@app.route('/respondent-num-correct-mc-questions')
+def get_respondent_num_correct_mc_questions():
+  creator = request.args.get('respondent-id')
+  cur = db_connection.get_db().cursor()
+  cur.execute("""select count(*) as num from Respondents 
+                                 natural join FormsRespondentsQuestions
+                                 natural join Questions
+                                 join MCQuestions on MCQuestionId = Questions.QuestionId
+                                 natural join MCQuestionPossibilities where MCQuestionCorrect = 1 and RespondentUsername = '""" + str(creator) +"'")
+  row_headers = [x[0] for x in cur.description]
+  json_data = []
+  theData = cur.fetchall()
+  for row in theData:
+      json_data.append(dict(zip(row_headers, row)))
+  return jsonify(json_data)
+
+@app.route('/respondent-num-wrong-mc-questions')
+def get_respondent_num_wrong_mc_questions():
+  creator = request.args.get('respondent-id')
+  cur = db_connection.get_db().cursor()
+  cur.execute("""select count(*) as num from Respondents 
+                                 natural join FormsRespondentsQuestions
+                                 natural join Questions
+                                 join MCQuestions on Questions.QuestionId = MCQuestionId
+                                 natural join MCQuestionPossibilities where MCQuestionCorrect = 0 and RespondentUsername = '""" + str(creator) +"'")
   row_headers = [x[0] for x in cur.description]
   json_data = []
   theData = cur.fetchall()
